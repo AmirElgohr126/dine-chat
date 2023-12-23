8cff918883762d9609b9af1a49cbbf4d2c874c41
Amirnagy <amirnagy886@gmail.com>
2023-12-23 21:57:13 +0200

    udpate user request

--- app/Http/Requests/Dashboard/Auth/LoginRestaurantRequest.php
+++ app/Http/Requests/Dashboard/Auth/LoginRestaurantRequest.php
@@ -22,7 +22,7 @@ class LoginRestaurantRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email|unique:restaurant_users,email|max:255',
+            'email' => 'required|email|exists:restaurant_users,eamil,max:255',
             'password' => 'required|string'
         ];
     }
